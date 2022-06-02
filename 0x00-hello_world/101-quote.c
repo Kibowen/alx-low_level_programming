9246f2c4a056f140f0e30101215b266bea152f17
Trevor <chelimotrevorkibowen@gmail.com>
2022-06-03 00:06:41 +0300

    advance tasks.

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,15 +2,16 @@
 #include <unistd.h>
 
 /**
- * main - Entry point
+ * main - use to return the integer value 1
  *
- * Description: 'and that piece of art is useful" - Dora Korpar, 2015-10-19'
+ * Description: use to print to standard output in the main function
  *
- * Return: Always 0 (Success)
+ * Return: return the value 1
+ *
  */
 
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
