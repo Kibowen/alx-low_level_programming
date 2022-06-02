55018c964415c66fd1f4bd51f34dc58fcfe877c9
Kibowen <Kibowen>
2022-06-02 03:39:26 -0700

    correction on no 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
 
-
-/**
+/**                                                                          
  *
- *main - Entry point
+ * main - Entry point                                                        
  *
+ *                                                                           
  *
+ * Return: Always 0 (Success)                                                
  *
- *Return: Always 0 (Success)
+ *                                                                           
  *
- *
- */
+ */   
 
 
 
@@ -21,6 +21,6 @@ int main(void)
 
 		puts("\"Programming is like building a multilingual puzzle");
 
-		return (0);
+			return (0);
 
 }
