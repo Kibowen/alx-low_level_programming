59ba31f11e4366071db62683bd4c58a469312845
Trevor <chelimotrevorkibowen@gmail.com>
2022-06-07 23:06:03 +0300

    task 1 corrections

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,13 +5,15 @@
  * return: always (0) success
  */
 
-int main()
+int main(void)
 {
     char ch[8]= "_putchar";
     int m;
-    
-    for(m = 0; m < 8; m++)
-        putchar(ch);
 
+    for(m = 0; m < 8; m++)
+        _putchar(ch);
+
+    _putchar(\n);
+    
     return (0);
 }
