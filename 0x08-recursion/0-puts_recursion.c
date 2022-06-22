7877a31b25b615533de83655e387993ea9da65f1
Trevor <chelimotrevorkibowen@gmail.com>
2022-06-22 17:13:47 +0300

    my first commit

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,17 +3,17 @@
 /**
  * _puts_recursion - function that prints a string 
  * 
- * @s: is a charcter string
+ * @s: This is given
  * 
- * Return: void
+ * Return: 0 if successul exit
  */
 
 void _puts_recursion(char *s)
 {
     if (*s == '\0')
     {
-        putchar('\n');
-        return 0;
+        _putchar('\n');
+        return;
     }
     putchar(*s);
     _puts_recursion(s+1);
