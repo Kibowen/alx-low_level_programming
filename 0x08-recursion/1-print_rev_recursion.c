19a045728fc1ad6f1f3d0e24c1934f7ec94a2c82
Trevor <chelimotrevorkibowen@gmail.com>
2022-06-22 18:36:35 +0300

    damn white spaces

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - function that prints a string
+ * @s: This is given
+ * Return: 0 if successul exit
+ */
+
+void _print_rev_recursion(char *s)
+{
+if (*s == '\0')
+{
+return;
+}
+_print_rev_recursion(s + 1);
+printf("\n");
+_putchar(*s);
+}
---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,7 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - function that prints a string
+ * @s: This is given
+ * Return: 0 if successul exit
+ */
