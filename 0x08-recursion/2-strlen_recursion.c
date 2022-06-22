93c93a44d9a65067cac6454747bc21e1d6b39819
Trevor <chelimotrevorkibowen@gmail.com>
2022-06-22 19:34:02 +0300

    qn 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,7 +1,16 @@
 #include "main.h"
 
 /**
- * _puts_recursion - function that prints a string
+ * _strlen_recursion - function that prints number of strings
  * @s: This is given
  * Return: 0 if successul exit
  */
+
+int _strlen_recursion(char *s)
+{
+    if (*s == '\0') 
+    {
+        return (0);
+    }
+    return 1 + _strlen_recursion(s + 1);
+}
