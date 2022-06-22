cbd9ace5f3c8b26ecb478d0a37c79c8546b14682
Trevor <chelimotrevorkibowen@gmail.com>
2022-06-22 17:23:08 +0300

    damn white spaces

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,19 +2,17 @@
 
 /**
  * _puts_recursion - function that prints a string
- * 
  * @s: This is given
- * 
  * Return: 0 if successul exit
  */
 
 void _puts_recursion(char *s)
 {
-    if (*s == '\0')
-    {
-        _putchar('\n');
-        return;
-    }
-    _putchar(*s);
-    _puts_recursion(s+1);
+if (*s == '\0')
+{
+_putchar('\n');
+return;
+}
+_putchar(*s);
+_puts_recursion(s+1);
 }
