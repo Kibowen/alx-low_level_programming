d9f39de0f15165392b691bbad42c46e41abc5eb9
Trevor <chelimotrevorkibowen@gmail.com>
2022-06-28 12:56:47 +0300

    ha! question 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * *_strdup - function returns a pointer to a new string which is a duplicate of the string
+ * @str: char to be initialized with
+ * Return: pointer to array or null
+ **/
+
+char *_strdup(char *str)
+{
+	char *strDup;
+	int i, j;
+
+	if (str == NULL)
+		return (NULL);
+	
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		strDup = malloc(sizeof (*str) * i);
+	}
+	if (strDup == NULL)
+		return (NULL);
+	
+	for (j = 0; str[j] != '\0'; j++)
+	{
+		strDup[j] = str[j];
+	}
+	return (strDup);
+}
---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,8 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * *create_array - creates an array of chars, and initializes it with a char
+ * @size: size of the array
+ * @c: char to be initialized with
+ * Return: pointer to array or null
+ **/
