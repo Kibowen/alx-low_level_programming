af72f4b9bdc0606cb907fa2355b5cd8203cbfdaf
Trevor <chelimotrevorkibowen@gmail.com>
2022-06-28 13:31:38 +0300

    mandatory finished

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -2,7 +2,16 @@
 #include <stdlib.h>
 /**
  * *create_array - creates an array of chars, and initializes it with a char
- * @size: size of the array
- * @c: char to be initialized with
+ * @grid: arrays in the grid
+ * @height: row count
  * Return: pointer to array or null
  **/
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+	free(grid);
+}
