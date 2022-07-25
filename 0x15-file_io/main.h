b731afab14db4e4a31c4462bfe819f5cf44323e4
Trevor <chelimotrevorkibowen@gmail.com>
2022-07-25 22:06:06 +0300

    advanced projects.

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,14 @@
 #ifndef _MAIN
 #define _MAIN
 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <elf.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
 int _putchar(char c);
 
 ssize_t read_textfile(const char *filename, size_t letters);
